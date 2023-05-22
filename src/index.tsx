751f0dc0a07888f1741ec3b96af65bc6152cfaef
unknown <PooriaGg>
2023-05-22 11:14:21 +0330

    second

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='Ethereum-Explorer'>
+    <BrowserRouter basename='EtherScan'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
